b3761bea4f3a6c8574ca477a15bbfd0d287c9ed6
xomimishka <mskliarova@sfedu.ru>
2026-02-13 17:51:04 +0300

    собрала проект в докер

---
+++ server/init.sql
@@ -0,0 +1,52 @@
+-- USERS
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id SERIAL PRIMARY KEY,
+    login VARCHAR(255) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    auth_provider VARCHAR(255)
+);
+
+-- LINKS
+CREATE TABLE IF NOT EXISTS public.links
+(
+    id SERIAL PRIMARY KEY,
+    "long" TEXT NOT NULL,
+    short VARCHAR(6) NOT NULL,
+    type BOOLEAN DEFAULT false,
+    qr TEXT,
+    valid_from TIMESTAMPTZ,
+    valid_until TIMESTAMPTZ,
+    has_end_date BOOLEAN DEFAULT false
+);
+
+CREATE INDEX IF NOT EXISTS idx_short
+    ON public.links(short);
+
+-- UNSAFE LINKS
+CREATE TABLE IF NOT EXISTS public.unsafe_links
+(
+    "long" TEXT NOT NULL UNIQUE
+);
+
+-- USER_LINKS
+CREATE TABLE IF NOT EXISTS public.user_links
+(
+    user_id INTEGER NOT NULL,
+    link_id INTEGER NOT NULL,
+    title VARCHAR(255),
+    is_favorite BOOLEAN DEFAULT false,
+    PRIMARY KEY (user_id, link_id),
+    FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE,
+    FOREIGN KEY (link_id) REFERENCES public.links(id) ON DELETE CASCADE
+);
+
+-- CLICK_STATS
+CREATE TABLE IF NOT EXISTS public.click_stats
+(
+    link_id INTEGER,
+    country VARCHAR(255),
+    city VARCHAR(255),
+    created_at TIMESTAMPTZ DEFAULT now(),
+    FOREIGN KEY (link_id) REFERENCES public.links(id) ON DELETE CASCADE
+);
